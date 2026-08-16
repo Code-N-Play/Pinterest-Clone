5941747aa01c3d2d4216e1e7534ff58ff431a15a
Suraj2002fake <neerajkumarmaurya248@gmail.com>
2026-08-16 15:52:26 +0530

    pintereset cloning start

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose").default;
+
+mongoose.connect ("mongodb://127.0.0.1:27017/pinterestdb");
+
+const userschema = mongoose.Schema({
+  username: String,
+  name:String,
+  email: String,
+  contact: Number,
+  password : String,
+  dob:{
+    type: Date,
+    default:Date.now()
+  },
+  boards:{
+    type: Array,
+    default: []
+  }
+});
+
+userschema.plugin(plm);
+
+module.exports = mongoose.model("login" , userschema);
